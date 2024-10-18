c9cf2910a8b5ec7166ee35fe880816c466e41cf0
Your Name <you@example.com>
2024-10-18 18:55:03 +0930

    first commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = ">= 0.7.0"
+    }
+  }
+}
+
+resource "libvirt_domain" "virt-machine" {
+  count  = var.vm_count
+  name   = format("${var.vm_hostname_prefix}%02d", count.index + var.index_start)
+  memory = var.memory
+  cpu {
+    mode = var.cpu_mode
+  }
+  vcpu       = var.vcpu
+  autostart  = var.autostart
+  firmware   = var.firmware
+
+  network_interface {
+    bridge         = var.bridge
+    wait_for_lease = false
+    hostname       = format("${var.vm_hostname_prefix}%02d", count.index + var.index_start)
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  graphics {
+    type        = "vnc"
+    listen_type = "address"
+    autoport    = true
+  }
+
+  disk {
+    volume_id = element(libvirt_volume.volume-qcow2[*].id, count.index)
+  }
+
+  dynamic "disk" {
+    for_each = var.additional_disk_ids
+    content {
+      volume_id = disk.value
+    }
+  }
+
+  dynamic "filesystem" {
+    for_each = var.share_filesystem.source != null ? [var.share_filesystem.source] : []
+    content {
+      source     = var.share_filesystem.source
+      target     = var.share_filesystem.target
+      readonly   = var.share_filesystem.readonly
+      accessmode = var.share_filesystem.mode
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,143 @@
+variable "os_img_url" {
+  description = "URL to the OS image"
+  type        = string
+  default     = "/var/lib/libvirt/images/nixos.qcow2"
+}
+
+variable "dhcp" {
+  description = "Use DHCP or Static IP settings"
+  type        = bool
+  default     = true
+}
+
+variable "bridge" {
+  description = "Bridge interface"
+  type        = string
+  default     = "br0"
+}
+
+variable "base_volume_name" {
+  description = "Name of base OS image"
+  type        = string
+  default     = null
+}
+
+variable "base_pool_name" {
+  description = "Name of base OS image"
+  type        = string
+  default     = null
+}
+
+variable "additional_disk_ids" {
+  description = "List of volume ids"
+  type        = list(string)
+  default     = []
+}
+
+
+variable "autostart" {
+  description = "Autostart the domain"
+  type        = bool
+  default     = true
+}
+
+variable "firmware" {
+  description = "Path to the OVMF firmware file for UEFI"
+  type        = string
+  default     = "/run/libvirt/nix-ovmf/OVMF_CODE.fd"
+}
+
+variable "vm_count" {
+  description = "Number of VMs"
+  type        = number
+  default     = 1
+}
+
+variable "index_start" {
+  description = "From where the indexig start"
+  type        = number
+  default     = 1
+}
+
+variable "vm_hostname_prefix" {
+  description = "VM hostname prefix"
+  type        = string
+  default     = "vm"
+}
+
+variable "memory" {
+  description = "RAM in MB"
+  type        = string
+  default     = "1024"
+}
+
+variable "cpu_mode" {
+  description = "CPU mode"
+  type        = string
+  default     = "host-passthrough"
+}
+
+variable "xml_override" {
+  description = "With these variables you can: Enable hugepages; Set USB controllers; Attach USB devices"
+  type = object({
+    hugepages = bool
+    usb_controllers = list(object({
+      model = string
+    }))
+    usb_devices = list(object({
+      vendor  = string
+      product = string
+    }))
+  })
+  default = {
+
+    hugepages = false,
+
+    usb_controllers = [
+      {
+        model = "piix3-uhci"
+      }
+    ],
+
+    usb_devices = [
+      # {
+      #   vendor = "0x0123",
+      #   product = "0xabcd"
+      # }
+    ]
+  }
+
+}
+
+variable "vcpu" {
+  description = "Number of vCPUs"
+  type        = number
+  default     = 1
+}
+
+variable "pool" {
+  description = "Storage pool name"
+  type        = string
+  default     = "default"
+}
+
+variable "system_volume" {
+  description = "System Volume size (GB)"
+  type        = number
+  default     = 10
+}
+
+variable "share_filesystem" {
+  type = object({
+    source   = string
+    target   = string
+    readonly = bool
+    mode     = string
+  })
+  default = {
+    source   = null
+    target   = null
+    readonly = false
+    mode     = null
+  }
+}
